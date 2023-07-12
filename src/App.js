dd4f282aec008de41cb5c916a3ecd0a828c59b5b
exprodev <k191115@nu.edu.pk>
2023-07-13 03:04:11 +0500

    New

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import myPic from './assets/Shehryar.jpg'
+import resumeFile from "./assets/mShehryarResume.pdf";
 
 function App() {
+
+  function downloadFile() {
+    const link = document.createElement("a");
+    link.href = resumeFile;
+    link.download = "mShehryarResume.pdf";
+    link.target = "_blank";
+    link.click();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="Intro">
+        <nav className='Navbar'>Welcome, to</nav>
+        <div className="Name">M SHEHRYAR AHMED's</div>
+        <div className='Subtext'>Portfolio</div>
+      </div>
+      <div className='pic-container'>
+        <img src={myPic} alt='Shehryar' className='centerPic' />
+      </div>
+      <div className='Summary'>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <q>
+            I am a passionate software engineer with a focus on full-stack web development. I have experience in planning, designing, and developing applications to meet client requirements. My interests also include quantum computers and the potential of artificial intelligence and machine learning. I have developed novel applications and models in these areas. ReactJS is my preferred JavaScript framework.
+          </q>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className='resume-section'>
+        <button className='resume-button' onClick={() => downloadFile("mShehryarResume.pdf")}><i className="fa fa-cloud-download"></i> Resume</button>
+      </div>
+      <div className='Socials'>
+        <p>More About Me!</p>
+        <p className='social-icons'>
+          <a target="_blank" href="https://www.linkedin.com/in/mohammedshehryarahmed/" className="fa fa-linkedin"></a>
+          <a target="_blank" href="https://github.com/exprodev" className="fa fa-github"></a>
+          <a target="_blank" href="https://www.facebook.com/mohammedshehryarahmed" className="fa fa-facebook"></a>
+          <a target="_blank" href="https://yaarkidaastan.blogspot.com/" className="blogger-icon"></a>
+          <a target="_blank" href="https://twitter.com/msherryahmed" className="fa fa-twitter"></a>
+          <a target="_blank" href="https://www.instagram.com/m.shehryarahmed/" className="fa fa-instagram"></a>
+        </p>
+      </div>
     </div>
   );
 }
